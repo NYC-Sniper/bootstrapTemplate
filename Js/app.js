7458b0adbaedea59764027675ed54d47412de7f2
Mohamed Hidji <68740384+medohidji@users.noreply.github.com>
2020-09-23 00:06:44 +0100

    First initialization

---
+++ Js/app.js
@@ -0,0 +1,12 @@
+window.onscroll = function () {
+    mainHeader()
+};
+
+function mainHeader() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        document.getElementById("navi").className = "navigation blur sticky-top";
+    } else {
+        document.getElementById("navi").className = "navigation";
+    }
+    
+};
