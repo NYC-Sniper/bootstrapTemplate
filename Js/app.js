5108ff3ae34e210ca39b9a1a771b0a93bfd56ef2
Mohamed Hidji <68740384+medohidji@users.noreply.github.com>
2020-09-23 18:51:29 +0100

    added animations

--- Js/app.js
+++ Js/app.js
@@ -1,3 +1,43 @@
+gsap.from('.title', {
+    opacity: 0,
+    stagger: 0.2,
+    duration: 1.2,
+    x: -90,
+});
+
+gsap.from('.image', {
+    opacity: 0,
+    duration: 1.2,
+    x: 90
+});
+const wordsArray = ["We are Design Duty", "We are a UI/UX Agency."];
+
+let cursor = gsap.to('.cursor', {
+opacity: 0,
+repeat: -1,
+ease: "power2.inOut",
+duration: .7
+})
+
+gsap.registerPlugin(TextPlugin)
+
+let wordsTimeline = gsap.timeline({
+repeat: -1,
+});
+wordsArray.forEach(word => {
+let i = gsap.timeline({
+    repeat: 1,
+    yoyo: true,
+    delay: 1, 
+    repeatDelay: 1
+});
+i.to(".textArray", {
+    duration: 1, 
+    text: word,
+});
+wordsTimeline.add(i);
+});
+
 window.onscroll = function () {
     mainHeader()
 };
