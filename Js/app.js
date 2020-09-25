899a4e776cf24f6970e7ec7bbeb52ca0b44cec68
Mohamed Hidji <68740384+medohidji@users.noreply.github.com>
2020-09-25 19:54:09 +0100

    New Update

--- Js/app.js
+++ Js/app.js
@@ -1,55 +1,19 @@
-// animations
-// animating the hero section
-gsap.from('.hero-title', {
+gsap.from(".title", {
     opacity: 0,
-    stagger: 0.2,
-    duration: 1.2,
     x: -90,
-});
-
-// animating the hero image
-gsap.from(".hero-image", {
+    duration: 2, 
+})
+gsap.from(".partTwo", {
+    opacity: 0,
     x: 90,
-    duration: 1.2,
+    duration: 2, 
+})
+gsap.from(".navbar", {
     opacity: 0,
+    y: -90,
+    duration: 2, 
 })
 
-// self-typing words
-const wordsArray = ["UI Design", "User Experience", "Brand Identity", "Everything"];
-
-// animating the cursor
-let cursor = gsap.to('.cursor', {
-opacity: 0,
-repeat: -1,
-ease: "power2.inOut",
-duration: .7
-})
-
-gsap.registerPlugin(TextPlugin)
-
-let wordsTimeline = gsap.timeline({
-repeat: -1,
-});
-wordsArray.forEach(word => {
-let i = gsap.timeline({
-    repeat: 1,
-    yoyo: true,
-    delay: 1, 
-    repeatDelay: 1
-});
-i.to(".words", {
-    duration: 1, 
-    text: word,
-});
-wordsTimeline.add(i);
-});
-
-// animating the navbar
-gsap.from('.navigation', {
-    duration: 1,
-    opacity: 0,
-    y: -90
-})
 
 // add blur class to navbar
 window.onscroll = function () {
@@ -58,8 +22,8 @@ window.onscroll = function () {
 
 function mainHeader() {
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        document.getElementById("navi").className = "navigation blur sticky-top";
+        document.getElementById("navi").className = "navbar blur sticky-top navbar-expand-lg navbar-light";
     } else {
-        document.getElementById("navi").className = "navigation";
+        document.getElementById("navi").className = "navbar navbar-expand-lg navbar-light";
     }
 };
