001da6e205c722b17c4451797dd84a83fb6709e6
Mohamed Hidji <68740384+medohidji@users.noreply.github.com>
2020-09-25 23:34:18 +0100

    jn

--- Js/app.js
+++ Js/app.js
@@ -1,17 +1,46 @@
-gsap.from(".title", {
+// animating the first section
+gsap.from(".h-title", {
     opacity: 0,
     x: -90,
     duration: 2, 
+    stagger: 0.3,
+    delay: 1
 })
+// animating snd part
 gsap.from(".partTwo", {
     opacity: 0,
     x: 90,
     duration: 2, 
 })
+// animating the navbar
 gsap.from(".navbar", {
     opacity: 0,
     y: -90,
     duration: 2, 
+})
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from(".c-cards", {
+    scrollTrigger: {
+        trigger: ".c-cards",
+        toggleActions: "play resume reverse restart",
+    },
+    stagger: 0.7,
+    ease: "ease", 
+    delay: 1, 
+    x: -90,
+    duration: 2,
+    opacity: 0,
+})
+
+gsap.from(".c-card", {
+    stagger: 0.2,
+    ease: "ease", 
+    delay: 1, 
+    x: -90,
+    duration: 1,
+    opacity: 0,
 })
 
 
